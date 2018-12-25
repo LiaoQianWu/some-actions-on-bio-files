cf87281862eabcb9ab5ae752aa256f8c307785d1
LiaoQianWu <45515238+LiaoQianWu@users.noreply.github.com>
2018-12-25 21:14:42 +0800

    Add files via upload

---
+++ count.py
@@ -0,0 +1,25 @@
+#Type a file name we are gonna input on filename
+#Type a file name we are gonna output on newfilename
+
+with open("filename", "r") as f:
+    with open("newfilename", "w") as fh:
+        name = []
+        seq = []
+        for line in f:
+            if line.startswith(">"):
+                line = line.split()[0]
+                name.append(line)
+                seq.append("")
+                order = name.index(line)
+            else:
+                line = line.strip()
+                seq[order] += line
+                
+        for i in range(len(name)):
+            count_A = seq[i].count("A")
+            count_T = seq[i].count("T")
+            count_C = seq[i].count("C")
+            count_G = seq[i].count("G")
+            print ("%s: %s bp" % (name[i], len(seq[i])), file = fh)
+            print ("A -> %d\nT -> %d\nC -> %d\nG -> %d" %\
+                   (count_A, count_T, count_C, count_G), file = fh)
